55ef598ed494bf41453013209f34b92defe447a4
shiro <shiro.mns@gmail.com>
2020-11-05 12:02:35 +0900

    [リファクタリング]行末スペース、空行のインデントを削除

--- app/controllers/concerns/my_utility.rb
+++ app/controllers/concerns/my_utility.rb
@@ -55,10 +55,10 @@ module MyUtility
         if text && text.match("/")
           texts_or = text.split("/")
           for text_or in texts_or do
-            reference_word_set(params, data_name, text_or, match_suffix, "any") 
+            reference_word_set(params, data_name, text_or, match_suffix, "any")
           end
         else
-          reference_word_set(params, data_name, text, match_suffix, "all") 
+          reference_word_set(params, data_name, text, match_suffix, "all")
         end
       end
     end
@@ -78,21 +78,21 @@ module MyUtility
       operator_suffix = "all"
       data_name = data_name.gsub(/_or_/, "_and_")
     end
-      
+
     if text[0] == "\"" && text[-1] == "\""
       # 完全一致に添字を変更
       text.slice!(0,1)
       text.slice!(-1,1)
       match_suffix = "eq"
     end
-      
+
     param_push(params, data_name + "_" + not_suffix + match_suffix + "_" + operator_suffix, text)
   end
 
   # 検索文字列を分割し、Ransackが参照する配列に割り当てる
   def reference_concat_assign(params, data_name, param_key)
     return unless params[param_key]
-    
+
     match_suffix = "cont"
 
     texts = (params[param_key].include?(' ')) ? params[param_key].gsub(/[“”]/, "\"").split(" ") : [params[param_key].dup.gsub(/[“”]/, "\"")]
@@ -102,10 +102,10 @@ module MyUtility
         if text && text.match("/")
           texts_or = text.split("/")
           for text_or in texts_or do
-            reference_concat_set(params, data_name, text_or, match_suffix, "any") 
+            reference_concat_set(params, data_name, text_or, match_suffix, "any")
           end
         else
-          reference_concat_set(params, data_name, text, match_suffix, "all") 
+          reference_concat_set(params, data_name, text, match_suffix, "all")
         end
       end
     end
@@ -124,7 +124,7 @@ module MyUtility
       operator_suffix = "all"
       data_name = data_name.gsub(/_or_/, "_and_")
     end
-      
+
     param_push(params, data_name + "_" + not_suffix + match_suffix + "_" + operator_suffix, text)
   end
 
@@ -138,10 +138,10 @@ module MyUtility
       for text in texts do
         if text && text.match(/([\-\.\d]+)~([\-\.\d]+)/)
           text_match = text.match(/([\-\.\d]+)~([\-\.\d]+)/)
-          reference_number_set(params, data_name, text_match[0] + "~") 
-          reference_number_set(params, data_name, "~" + text_match[2]) 
+          reference_number_set(params, data_name, text_match[0] + "~")
+          reference_number_set(params, data_name, "~" + text_match[2])
         else
-          reference_number_set(params, data_name, text) 
+          reference_number_set(params, data_name, text)
         end
       end
     end
@@ -154,16 +154,16 @@ module MyUtility
       text.slice!(0,1)
       match_suffix = "lteq"
     end
-      
+
     if text[-1] == "~"
       text.slice!(-1,1)
       match_suffix = "gteq"
     end
-      
+
     param_push(params, data_name + "_" + match_suffix + "_any", text)
   end
- 
-  # Ransackの検索用パラメータに追加。配列がない場合は作成する 
+
+  # Ransackの検索用パラメータに追加。配列がない場合は作成する
   def param_push(params, ransack_param, text)
     unless params[:q][ransack_param].is_a?(Array)
       params[:q][ransack_param] = Array.new
@@ -193,7 +193,7 @@ module MyUtility
 
         # 絞り込んだ結果ヒット件数が0になった場合、ダミー値で絞り込ませる
         # 　0件で配列を返すと、絞り込みが行われず全件ヒットするため
-        if detection_arrays[:and].length == 0 then 
+        if detection_arrays[:and].length == 0 then
           detection_arrays[:and] = [-99999]
           return
         end
@@ -251,7 +251,7 @@ module MyUtility
       params[:q]["place_area_eq_any"] = place_array
       params[:q]["place_field_id_eq_any"] = Place.pc_to_field_id(params_tmp)
     end
-      
+
     form_params["place_result_no_form"] = params["place_result_no_form"]
     form_params["place_e_no_form"] = params["place_e_no_form"]
     form_params["place_pc_name_form"] = params["place_pc_name_form"]
